9833fbdd1a00b32aa0ef48cd926009c0f092df76
Aditya <aadityakum123@gmail.com>
2023-10-23 12:18:32 +0530

    styled

---
+++ p12.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $url = 'https://learn.gitam.edu/pluginfile.php/988491/mod_resource/content/1/Unit%202.pdf';
+    $file_name = basename($url);
+
+    if(file_put_contents($file_name , file_get_contents($url))){
+        echo "File downloaded successfully";
+        echo"<br> $file_name";
+        }else{
+        echo "File downloading failed.";
+        }
+    
+    ?>
+</body>
+</html>
